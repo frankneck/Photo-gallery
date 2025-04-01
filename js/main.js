3e699979240aa90cd0c5ecc59cfd9b9f995ec0d7
frankneck13 <finn.f1rst128@gmail.com>
2025-04-01 11:11:22 +1000

    What: I've made a work with tag fully.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var colNum = 1;
 var tags = [] // list of all tags
+let uploadedImages = new Set();
 
 function findAllTags() {
     var allTags = document.getElementsByClassName("button-tag") // all objects who is button
@@ -14,7 +15,7 @@ function findAllTags() {
 
 function addNewTag(newTag) {
     
-    if (!tags.includes(newTag)) {
+    if (!(tags.includes(newTag)) && newTag.length > 0) {
         tags.push(newTag);
         const selects = document.getElementsByName("selectTag");
         
@@ -35,38 +36,94 @@ function addNewTag(newTag) {
 }
 
 
-function addElement(imageInput, description, tag1 = "none", tag2 = "none", tag3 = "none") {
-    var cols = document.getElementsByClassName("column"); // получаем все столбцы
+function addElement(imageInput, description, tag1 = "None", tag2 = "None", tag3 = "None") {
+    var cols = document.getElementsByClassName("column");                                       // получаем все столбцы
     var newItem = document.createElement("figure");
+    let tagButtons = "";
 
     // Получаем файл из input
-    var image = imageInput.files[0]; // берем первый файл
-
-    // Проверяем, что файл существует
-    if (!image) {
+    var image = imageInput.files[0];                                                            // берем первый файл
+                
+    if (!image) {                                                                               // Проверяем, что файл существует
         alert("Please select a file.");
+        return;
+    }
+    
+    if (tag1 === "None" && tag2 === "None" && tag3 === "None") {
+        alert("Choose at least one tag!");
         return;
     }
 
     var reader = new FileReader();
     
+    if (tag1 === tag2 && tag2 === tag3) {
+        [tag1].forEach((tag) => {
+            if (tag !== "None") {                                               // если теги разные и нер равны None
+                    tagButtons += `
+                        <button class="button-tag" id="tag1" name="tag">
+                            <a href="#">${tag}</a>
+                        </button>`
+                }
+            });                                                                     // если теги одинаковые, добавляем просто первый
+    }
+    else if (tag1 === tag2 && tag2 !== tag3) {
+        [tag1, tag3].forEach((tag) => {
+            if (tag !== "None") {                                               // если теги разные и нер равны None
+                    tagButtons += `
+                        <button class="button-tag" id="tag1" name="tag">
+                            <a href="#">${tag}</a>
+                        </button>`
+                }
+        });
+    }
+    else if (tag1 === tag3 && tag1 !== tag2) {
+        [tag1, tag2].forEach((tag) => {
+            if (tag !== "None") {                                               // если теги разные и нер равны None
+                    tagButtons += `
+                        <button class="button-tag" id="tag1" name="tag">
+                            <a href="#">${tag}</a>
+                        </button>`
+                }
+            });
+    }
+    else if (tag2 === tag3 && tag2 !== tag1) {
+        [tag1, tag3].forEach((tag) => {
+            if (tag !== "None") {                                               // если теги разные и нер равны None
+                    tagButtons += `
+                        <button class="button-tag" id="tag1" name="tag">
+                            <a href="#">${tag}</a>
+                        </button>`
+                }
+            });
+    }
+    else if (tag1 !== tag2 && tag2 !== tag3 && tag1 !== tag3) {
+        [tag1, tag2, tag3].forEach((tag) => {
+        if (tag !== "None") {                                               // если теги разные и нер равны None
+                tagButtons += `
+                    <button class="button-tag" id="tag1" name="tag">
+                        <a href="#">${tag}</a>
+                    </button>`
+            }
+        });
+    }
+
     reader.onload = function(e) {
-        var imageURL = e.target.result; // получаем результат чтения файла
+        const imageURL = e.target.result;                                   // получаем результат чтения файла
+        
+        if (uploadedImages.has(imageURL)) {
+            alert("This image has already been uploaded!");
+            return;
+        }
+
+        uploadedImages.add(imageURL);
+
         newItem.innerHTML = `
             <img src="${imageURL}" alt="Uploaded Image"/>
             <figcaption>
                 <p name="description" style="display: none;">
                     ${description}
                 </p>
-                <button class="button-tag" id="tag" name="tag">
-                    <a href="#">${tag1}</a>
-                </button>
-                <button class="button-tag" id="tag" name="tag">
-                    <a href="#">${tag2}</a>
-                </button>
-                <button class="button-tag" id="tag" name="tag">
-                    <a href="#">${tag3}</a>
-                </button>
+                ${tagButtons}
             </figcaption>
         `;
 
@@ -81,16 +138,13 @@ function addElement(imageInput, description, tag1 = "none", tag2 = "none", tag3 
                 minCol = col;
             }
         });
-
+        
         minCol.insertAdjacentElement("beforeend", newItem);
+        closeForm(); // если все успешно, закрываем форму
     };
 
     // Чтение изображения как DataURL
     reader.readAsDataURL(image);
-}
-
-function clickButtonAddElement() {
-
 }
 
 // фильтраиця тестовая версия
@@ -107,48 +161,20 @@ function filterPictureByTag() {
         });
     });
 
-    filteredPictures.forEach(function (picture) {
+    filteredPictures.forEach((picture) => {
+        
     });
 }
 
+// вспомогательные функции
 function openForm() {
     const form = document.querySelector("form");
     form.style.display = "block";
     form.reset();
-
-    // closing of the button
-    var count = 0;
-    var buttons = document.getElementsByName("AddElement");
-    
-    buttons.forEach( function (button) {
-
-        if (count == 1)
-        {
-            button.style.display = "none";
-        }
-        
-        count++;
-    });
-    
 }
 
 function closeForm() {
-    const form = document.getElementById("Adding form");
-    form.style.display = "none";
+const form = document.getElementById("Adding form");
+form.style.display = "none";
     form.reset();
-
-    // opening of the button
-    var count = 0;
-    var buttons = document.getElementsByName("AddElement");
-    
-    buttons.forEach( function (button) {
-
-        if (count == 1) {
-            button.style.display = "block";
-        }
-
-        count++;
-    });
 }
-
-findAllTags();
